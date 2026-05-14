1e1374320395bde976c5ceaa1523e2f2c29c0bb2
Mohammed Imran <mohammedimran86992@gmail.com>
2026-05-14 12:11:45 +0530

    Update query editor panel height to use h-70 class

--- src/components/query-editor-panel.tsx
+++ src/components/query-editor-panel.tsx
@@ -265,7 +265,7 @@ export function QueryEditorPanel({ tab, isClient }: QueryEditorPanelProps) {
         ) : null}
 
         <div className="flex min-h-0 flex-1 flex-col">
-          <div className="relative h-[17.5rem] shrink-0 border-b border-border-subtle bg-surface-app">
+          <div className="relative h-70 shrink-0 border-b border-border-subtle bg-surface-app">
             {isClient ? (
               <MonacoEditor
                 height="100%"
